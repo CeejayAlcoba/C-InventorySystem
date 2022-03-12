01bfb2b33dfed6fa501b186fcb3a05a0c7c03295
Criszon James Alcoba <smithkim909@gmail.com>
2022-03-13 00:13:12 +0800

    remove comment

--- WebApi/Startup.cs
+++ WebApi/Startup.cs
@@ -135,7 +135,6 @@ namespace WebApi
             else
             {
                 app.UseExceptionHandler("/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
             app.UseMiddleware<JWTMiddleware>();
