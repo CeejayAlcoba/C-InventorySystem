5f3ae4c30b4c7eaba283d95cf9a6a8dea9ad48c7
Criszon James Alcoba <smithkim909@gmail.com>
2022-03-22 22:23:37 +0800

    Add Product Fields

---
+++ DataAccessEFCore/Migrations/20220322142201_AddProductFields.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DataAccessEFCore.Migrations
+{
+    public partial class AddProductFields : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Code",
+                table: "Products",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Price",
+                table: "Products",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "TotalPrice",
+                table: "Products",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Code",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "Price",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "TotalPrice",
+                table: "Products");
+        }
+    }
+}
--- Domain/Entities/Product.cs
+++ Domain/Entities/Product.cs
@@ -11,8 +11,11 @@ namespace Domain.Entities
     {
         [Key]
         public int ProductId { get; set; }
+        public int Code { get; set; }
         public string ProductName { get; set; }
         public string Brand { get; set; }
-        public int Quantity { get; set; }   
+        public int Price { get; set; }
+        public int Quantity { get; set; } 
+        public int TotalPrice { get; set; }
     }
 }
